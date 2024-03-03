2d11e77cf733733ff0e4eb1adc7a6e0e32e71cf5
sabir.aliyev <sabir.aliyev.itdep@gmail.com>
2024-03-03 17:35:29 +0400

    Using Factory to create dummy Listings data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,28 +16,30 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(2)->create();
 
-        Listing::create(
-            [
-                'title' => 'Laravel Senior Developer',
-                'tags' => 'laravel, javascript',
-                'company' => 'Acme Inc.',
-                'location' => 'Boston',
-                'email' => 'email1@acme.com',
-                'website' => 'http://acme.com',
-                'description' => 'Professional Laravel developer with 5 years of experience. Must be able to work in a fast-paced environment.'
-            ]
-        );
+        Listing::factory(6)->create();
 
-        Listing::create(
-            [
-                'title' => 'Full Stack Developer',
-                'tags' => 'nodejs, react',
-                'company' => 'Innovatech Solutions',
-                'location' => 'San Francisco',
-                'email' => 'dev2@innovatech.com',
-                'website' => 'http://innovatech.com',
-                'description' => 'Seeking a full stack developer familiar with Node.js and React for building web applications.'
-            ]
-        );
+        // Listing::create(
+        //     [
+        //         'title' => 'Laravel Senior Developer',
+        //         'tags' => 'laravel, javascript',
+        //         'company' => 'Acme Inc.',
+        //         'location' => 'Boston',
+        //         'email' => 'email1@acme.com',
+        //         'website' => 'http://acme.com',
+        //         'description' => 'Professional Laravel developer with 5 years of experience. Must be able to work in a fast-paced environment.'
+        //     ]
+        // );
+
+        // Listing::create(
+        //     [
+        //         'title' => 'Full Stack Developer',
+        //         'tags' => 'nodejs, react',
+        //         'company' => 'Innovatech Solutions',
+        //         'location' => 'San Francisco',
+        //         'email' => 'dev2@innovatech.com',
+        //         'website' => 'http://innovatech.com',
+        //         'description' => 'Seeking a full stack developer familiar with Node.js and React for building web applications.'
+        //     ]
+        // );
     }
 }
